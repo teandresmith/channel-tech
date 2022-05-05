21887419b60956808dfd7d4df3258de0575a719b
Te'Andre Smith <teandre3@hotmail.com>
2022-05-05 14:00:14 +0900

    Migrated CustomAlert component to typescript

---
+++ react-frontend-service/src/components/CustomMUI/CustomAlert.tsx
@@ -0,0 +1,20 @@
+import { Alert, AlertColor, SxProps, Theme } from '@mui/material'
+import React from 'react'
+
+type CustomAlertProps = {
+  severity: AlertColor
+  sx: SxProps<Theme>
+  message: string
+}
+
+const CustomAlert = ({ severity, sx, message }: CustomAlertProps) => {
+  return (
+    <>
+      <Alert severity={severity} sx={sx}>
+        {message}
+      </Alert>
+    </>
+  )
+}
+
+export default CustomAlert
